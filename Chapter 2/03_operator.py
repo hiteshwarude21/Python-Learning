6a34e15c3356fb9a13fa1561cc20d3dd831a4e29
hiteshwarude21 <hiteshwarude2005@gmail.com>
2026-07-05 22:40:35 +0530

    Logical Operator in Python

--- Chapter 2/03_operator.py
+++ Chapter 2/03_operator.py
@@ -30,3 +30,24 @@ d = 5<3 #Check if 5 is less than 3
 # d = 5!=3 #Check if 5 is not equal to 3
 print(d)  
 
+
+# Logical Operators
+
+e = True or False #Check if both the values are True
+print(e)
+
+#truth table for OR operator
+print("True or false is: ", True or False) #True
+print("False or false is: ", False or False) #False    
+print("True or True is: ", True or True) #True
+print("False or True is: ", False or True) #True
+
+#truth table for AND operator
+print("True and false is: ", True and False) #False
+print("False and false is: ", False and False) #False
+print("True and True is: ", True and True) #True
+print("False and True is: ", False and True) #False
+
+
+
+
