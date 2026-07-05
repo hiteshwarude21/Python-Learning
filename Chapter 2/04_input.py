3f8f28efd227097e6e278c4c0bc865bc2ea765e2
hiteshwarude21 <hiteshwarude2005@gmail.com>
2026-07-05 23:14:44 +0530

    Taking input form the user in python

---
+++ Chapter 2/04_input.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number 1 : ")) 
+b = int(input("Enter a number 2 : "))
+
+print("Number a is: ", a) #print the value of a
+print("Number b is: ", b) #print the value of b
+
+print("sum is: ", a + b) 
