3ced5e659930e739ff7a72ad9a7768adb63ca496
hiteshwarude21 <hiteshwarude2005@gmail.com>
2026-07-06 10:01:12 +0530

    string and slicing in python

---
+++ Chapter 3/intro_to_strings.py
@@ -0,0 +1,12 @@
+a = 'Hitesh'
+
+b = "Hitesh"
+
+c = '''Hitesh'''
+
+print(a,b,c)
+
+name = "Hello"
+nameshort = name[0:3] # Start from index 0 and go up to index 3 
+                      #(not including 3)
+print(nameshort)
---
+++ Chapter 3/negative_slicing.py
@@ -0,0 +1,9 @@
+name = "Hello"
+
+print(name[0:3])
+
+print(name[-4: -1])
+print(name[1:4])
+print(name[:5])  # is sam as [0:5] because the default start index is 0
+print(name[1:])  # is same as [1:5] because the default end index is the length of the string
+print(name[1:5])
