3d5d5f6b80d331e088feb54e0662dca4fd6ae5a4
hiteshwarude21 <hiteshwarude2005@gmail.com>
2026-07-27 12:43:25 +0530

    Add escape_sqe_py

---
+++ Chapter 3/escape_sqe.py
@@ -0,0 +1,4 @@
+a = "Hello My Name is Hitesh Warude \nI am learning Python Programming"
+# \n is used to create a new line in the string. 
+# When this string is printed, the text after \n will appear on a new line.
+print(a)
