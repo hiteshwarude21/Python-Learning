7844ec60730da3570e8717da8f3a0bed42e1a544
hiteshwarude21 <hiteshwarude2005@gmail.com>
2026-07-27 13:24:22 +0530

    chapter 3 problem 3

---
+++ Chapter 3/Problem3_.py
@@ -0,0 +1,6 @@
+name = "Elon Musk is the richest man in  the world"
+
+print(name.find("richest"))
+  # This will return the index of the first occurrence of the substring "richest"
+print(name.find("  ")) #find the double space in the string
+
