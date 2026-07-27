8bcda9569c6b798f3fb61082f1e9897068a0cc6c
hiteshwarude21 <hiteshwarude2005@gmail.com>
2026-07-27 14:56:24 +0530

    tuple in python

---
+++ Chapter 4/tuple.py
@@ -0,0 +1,5 @@
+a = (12,32,43,54,65,"Rahul", "hitesh", 23.5, 45.6, True)
+
+print(a)
+
+print(type(a)) # This will print the type of the variable 'a', which is a tuple
---
+++ Chapter 4/tuple_methods.py
@@ -0,0 +1,9 @@
+a = (12,32,43,54,65,"Rahul", "hitesh", 23.5, 45.6, True)
+
+print(a)
+
+no = a.count(43) #return the number of times 43 is present in the tuple
+print(no)
+
+no = a.index(65) #return the index of the first occurrence of 65 in the tuple
+print(no)
