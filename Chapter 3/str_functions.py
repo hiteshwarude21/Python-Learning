b67baf1c241d52dfd555038ff566e6d4f8c7ed54
hiteshwarude21 <hiteshwarude2005@gmail.com>
2026-07-27 12:18:23 +0530

    initial commit

---
+++ Chapter 3/str_functions.py
@@ -0,0 +1,8 @@
+name = "Hitesh"
+
+print(len(name)) # This will print the length of the string "Hitesh"
+
+print(name.upper()) # This will print the string "Hitesh" in uppercase
+print(name.lower()) # This will print the string "Hitesh" in lowercase
+print(name.capitalize()) # This will print the string "Hitesh" with the first letter capitalized
+print(name.title()) # This will print the string "Hitesh" with each word capitalized
