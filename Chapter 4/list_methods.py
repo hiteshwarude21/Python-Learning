28780e50af70172c09b07fe0c2f80901923a5681
hiteshwarude21 <hiteshwarude2005@gmail.com>
2026-07-27 14:35:43 +0530

    methods in list

---
+++ Chapter 4/list_methods.py
@@ -0,0 +1,19 @@
+friends =["Hitesh", "Orange", 10, 20.5, True, "python" , "programming"]
+
+print(friends)
+
+friends.append("RCPIT") #append method is used to add an element at the end of the list
+print(friends)
+
+list = [12,23,42,34,2,43,5,3,2,53,7,8,45,9]
+list.sort() #sort method is used to sort the list in ascending order
+print(list)
+
+list.reverse() #reverse method is used to reverse the list
+print(list)
+
+list.insert(2, 2323232) #insert method is used to insert an element at a specific index in the list
+print(list)
+
+list.pop(3) # remove the element from index 3 
+print(list)
