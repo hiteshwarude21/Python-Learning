272d0ebba04c0ef3d5b1b448d74c6945e97feb2c
hiteshwarude21 <hiteshwarude2005@gmail.com>
2026-07-27 13:30:56 +0530

    problem 4 of chapter 3

---
+++ Chapter 3/Problem4_.py
@@ -0,0 +1,10 @@
+name = "Elon Musk is the richest man in  the world"
+
+print(name.replace("  ", " "))
+  # This will return the index of the first occurrence of the substring "richest"
+
+print(name.find("  ")) #find the double space in the string
+
+
+# String are immutable in python.it means 
+# we can not change them while runing function on them
