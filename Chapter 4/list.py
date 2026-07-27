2f9025ea59539d32d1064eaca7b305896b986537
hiteshwarude21 <hiteshwarude2005@gmail.com>
2026-07-27 14:14:09 +0530

    list in python

---
+++ Chapter 4/list.py
@@ -0,0 +1,8 @@
+friends =["Hitesh", "Orange", 10, 20.5, True, "python" , "programming"]
+
+print(friends[0])
+
+friends[0]="Red" #unlike strings list are mutable   
+print(friends[0])
+
+print(friends[1:4]) #slicing in list
