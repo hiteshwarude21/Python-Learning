bf3e93da928934144c00a01165777f49922009fc
hiteshwarude21 <hiteshwarude2005@gmail.com>
2026-07-28 15:15:17 +0530

    chapter 4 problem 1

---
+++ Chapter 4/Problem1_.py
@@ -0,0 +1,16 @@
+fruits=[]
+
+f1=input("Enter the name of fruit 1: ")
+fruits.append(f1)
+
+f2=input("Enter the name of fruit 2: ")
+fruits.append(f2)
+
+f3=input("Enter the name of fruit 3: ")
+fruits.append(f3)
+
+f4=input("Enter the name of fruit 4: ")
+fruits.append(f4)
+
+
+print("The list of fruits is:", fruits)
