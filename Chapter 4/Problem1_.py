f7fc617f310dafb6ece61249e0cd65df365cc1c9
hiteshwarude21 <hiteshwarude2005@gmail.com>
2026-07-28 15:27:22 +0530

    problem 2 of the chapter 4

--- Chapter 4/Problem1_.py
+++ Chapter 4/Problem1_.py
@@ -1,16 +1,18 @@
 fruits=[]
 
-f1=input("Enter the name of fruit 1: ")
+f1=int(input("Enter the name of fruit 1: "))
 fruits.append(f1)
 
-f2=input("Enter the name of fruit 2: ")
+f2=int(input("Enter the name of fruit 2: "))
 fruits.append(f2)
 
-f3=input("Enter the name of fruit 3: ")
+f3=int(input("Enter the name of fruit 3: "))
 fruits.append(f3)
 
-f4=input("Enter the name of fruit 4: ")
+f4=int(input("Enter the name of fruit 4: "))
 fruits.append(f4)
 
+fruits.sort()
 
 print("The list of fruits is:", fruits)
+
---
+++ Chapter 4/Problem2_.py
@@ -0,0 +1,18 @@
+Marks=[]
+
+f1=input("Enter the Marks 1: ")
+Marks.append(f1)
+
+f2=input("Enter the Marks 2: ")
+Marks.append(f2)
+
+f3=input("Enter the Marks 3: ")
+Marks.append(f3)
+
+f4=input("Enter the Marks 4: ")
+Marks.append(f4)
+
+Marks.sort()
+
+
+print("The list of Marks is:", Marks)
