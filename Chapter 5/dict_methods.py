e21d0ac0f29dfd0ce2b55cd44b7766ab9d45a069
hiteshwarude21 <hiteshwarude2005@gmail.com>
2026-07-28 16:42:45 +0530

    Dictionary methods in python

---
+++ Chapter 5/dict_methods.py
@@ -0,0 +1,21 @@
+marks = {
+    "Hitesh": 100,
+    "rahul": 90,
+    "Raj": 80,
+    "Ritesh": 20,
+}
+
+# print(marks.items())
+
+# print(marks.keys())
+
+# print(marks.values())
+
+# marks.update({"Hitesh":101,"Raj":90})
+# print(marks)
+
+
+print(marks.get("Hitesh2"))  # It gives the None 
+
+
+print(marks["Hitesh2"]) # it give the error
