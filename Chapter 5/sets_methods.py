02c5277143d0856649e1747f6f05dc6a875d0f19
hiteshwarude21 <hiteshwarude2005@gmail.com>
2026-07-30 13:26:08 +0530

    methods of set in python

---
+++ Chapter 5/sets_methods.py
@@ -0,0 +1,11 @@
+s = {12,23,343,3553,23,4534,5,34,4,"hitesh"}
+
+print(s,type(s))
+
+s.add(566)
+print(s,type(s))
+
+s.remove(3553)
+s.remove(4534)  # this remove only removes 1 element at time
+print(s,type(s))
+
